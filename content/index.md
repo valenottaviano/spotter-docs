d26eef08b6992b05fd12c64d98078e7190eeeea3
Valentin Ottaviano <valentinottaviano@gmail.com>
2023-11-11 11:39:43 -0300

    fist commit

---
+++ content/index.md
@@ -0,0 +1,5 @@
+---
+title: Docs | Spotter AI
+---
+Revolutionize your parking experience with our cutting-edge startup focused on security and efficiency. We leverage state-of-the-art QR code technology for seamless user validations, ensuring a swift and secure entry process. Our AI algorithms take parking optimization to the next level, intelligently managing parking spaces for maximum efficiency. Say goodbye to the hassle of finding a spot—welcome to the future of stress-free parking.
+
---
+++ quartz/components/Footer.tsx
@@ -0,0 +1,30 @@
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+
+interface Options {
+  links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  function Footer({ displayClass }: QuartzComponentProps) {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        <hr />
+        e
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    )
+  }
+
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
